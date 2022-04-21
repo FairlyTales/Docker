0665534ede42538948cc70cc201773296e0805c5
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-21 23:58:46 +0300

    prettier fix

--- 01-Building-an-Image/index.js
+++ 01-Building-an-Image/index.js
@@ -4,11 +4,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', async (req, res) => {
-    let image = await axios('https://source.unsplash.com/random');
-    res.send(`<img src="${image.request.res.responseUrl}" />`)
+	let image = await axios('https://source.unsplash.com/random');
+	res.send(`<img src="${image.request.res.responseUrl}" />`)
 })
 
 
 app.listen((3333), () => {
-    console.log("listening on port 3333!!!!!")
+	console.log("listening on port 3333")
 })
